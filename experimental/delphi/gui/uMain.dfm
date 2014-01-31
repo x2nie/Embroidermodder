d1cd25b0d1be14c749234f65ba93d42588af6711
x2nie <x2nie@yahoo.com>
2014-01-31 15:06:09 +0700

    One level target directory
    Target directory is only in the same dir of source.

--- experimental/delphi/gui/uMain.dfm
+++ experimental/delphi/gui/uMain.dfm
@@ -1,7 +1,7 @@
 object Form1: TForm1
-  Left = 133
-  Top = 130
-  Width = 928
+  Left = 127
+  Top = 194
+  Width = 1044
   Height = 480
   Caption = 'Embroidery Convert'
   Color = clBtnFace
@@ -15,14 +15,19 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object spl1: TSplitter
-    Left = 433
+    Left = 289
+    Top = 49
+    Height = 306
+  end
+  object spl2: TSplitter
+    Left = 561
     Top = 49
     Height = 306
   end
   object mmo1: TMemo
     Left = 0
     Top = 355
-    Width = 920
+    Width = 1036
     Height = 98
     Align = alBottom
     Lines.Strings = (
@@ -32,10 +37,10 @@ object Form1: TForm1
   object Panel1: TPanel
     Left = 0
     Top = 0
-    Width = 920
+    Width = 1036
     Height = 49
     Align = alTop
-    Caption = 'Panel1'
+    BevelOuter = bvNone
     TabOrder = 1
     object Button1: TButton
       Left = 96
@@ -58,7 +63,7 @@ object Form1: TForm1
   object vtOpen: TVirtualStringTree
     Left = 0
     Top = 49
-    Width = 433
+    Width = 289
     Height = 306
     Align = alLeft
     Header.AutoSizeIndex = 0
@@ -70,12 +75,13 @@ object Form1: TForm1
     Header.MainColumn = -1
     TabOrder = 2
     OnGetText = vtOpenGetText
+    OnGetNodeDataSize = vtOpenGetNodeDataSize
     Columns = <>
   end
-  object vtOutExt: TVirtualStringTree
-    Left = 436
+  object vtExts: TVirtualStringTree
+    Left = 292
     Top = 49
-    Width = 433
+    Width = 269
     Height = 306
     Align = alLeft
     Header.AutoSizeIndex = 0
@@ -84,12 +90,134 @@ object Form1: TForm1
     Header.Font.Height = -11
     Header.Font.Name = 'MS Sans Serif'
     Header.Font.Style = []
-    Header.MainColumn = -1
+    Header.Options = [hoColumnResize, hoDrag, hoShowSortGlyphs, hoVisible]
     TabOrder = 3
     TreeOptions.MiscOptions = [toAcceptOLEDrop, toCheckSupport, toFullRepaintOnResize, toInitOnSave, toToggleOnDblClick, toWheelPanning, toEditOnClick]
-    OnGetText = vtOutExtGetText
-    OnInitNode = vtOutExtInitNode
-    Columns = <>
+    TreeOptions.PaintOptions = [toShowButtons, toShowDropmark, toShowTreeLines, toThemeAware, toUseBlendedImages]
+    TreeOptions.SelectionOptions = [toFullRowSelect]
+    OnChecked = vtExtsChecked
+    OnGetText = vtExtsGetText
+    OnGetNodeDataSize = vtExtsGetNodeDataSize
+    OnInitNode = vtExtsInitNode
+    Columns = <
+      item
+        Position = 0
+        Width = 55
+        WideText = 'Format'
+      end
+      item
+        Position = 1
+        Width = 200
+        WideText = 'Description'
+      end>
+  end
+  object pnlOutput: TPanel
+    Left = 564
+    Top = 49
+    Width = 421
+    Height = 306
+    Align = alLeft
+    BevelOuter = bvNone
+    TabOrder = 4
+    object pnlOutputConfig: TPanel
+      Left = 0
+      Top = 0
+      Width = 421
+      Height = 185
+      Align = alTop
+      BevelOuter = bvNone
+      TabOrder = 0
+      DesignSize = (
+        421
+        185)
+      object rgAdditionalDir: TRadioGroup
+        Left = 16
+        Top = 104
+        Width = 393
+        Height = 57
+        Anchors = [akLeft, akTop, akRight]
+        Caption = 'Additional directory'
+        Columns = 3
+        ItemIndex = 2
+        Items.Strings = (
+          'Filename'
+          'File Extension'
+          'None')
+        TabOrder = 0
+      end
+      object grpTargetDir: TGroupBox
+        Left = 16
+        Top = 0
+        Width = 393
+        Height = 97
+        Anchors = [akLeft, akTop, akRight]
+        Caption = 'Target directory'
+        TabOrder = 1
+        DesignSize = (
+          393
+          97)
+        object rbInPlace: TRadioButton
+          Left = 8
+          Top = 24
+          Width = 185
+          Height = 17
+          Caption = 'In the same directory of source'
+          Checked = True
+          TabOrder = 0
+          TabStop = True
+        end
+        object rbOutputDifferentDir: TRadioButton
+          Left = 8
+          Top = 48
+          Width = 113
+          Height = 17
+          Caption = 'Other:'
+          TabOrder = 1
+        end
+        object edtOutputDir: TEdit
+          Left = 25
+          Top = 64
+          Width = 358
+          Height = 21
+          Anchors = [akLeft, akTop, akRight]
+          TabOrder = 2
+          OnChange = edtOutputDirChange
+        end
+      end
+    end
+    object vtResult: TVirtualStringTree
+      Left = 0
+      Top = 185
+      Width = 421
+      Height = 121
+      Align = alClient
+      Header.AutoSizeIndex = 0
+      Header.Font.Charset = DEFAULT_CHARSET
+      Header.Font.Color = clWindowText
+      Header.Font.Height = -11
+      Header.Font.Name = 'MS Sans Serif'
+      Header.Font.Style = []
+      Header.Options = [hoColumnResize, hoDrag, hoShowSortGlyphs, hoVisible]
+      TabOrder = 1
+      OnGetText = vtResultGetText
+      OnGetNodeDataSize = vtResultGetNodeDataSize
+      Columns = <
+        item
+          Position = 0
+          Width = 300
+          WideText = 'Output file'
+        end
+        item
+          Position = 1
+          Width = 100
+          WideText = 'Source File'
+        end
+        item
+          Position = 2
+          Width = 300
+          WideText = 'Source Path'
+        end>
+    end
   end
   object dlgOpen1: TOpenDialog
     Options = [ofHideReadOnly, ofAllowMultiSelect, ofEnableSizing]
--- experimental/delphi/gui/uMain.pas
+++ experimental/delphi/gui/uMain.pas
@@ -1,3 +1,56 @@
+{$A8,B-,C+,D+,E-,F-,G+,H+,I+,J-,K-,L+,M-,N+,O+,P+,Q-,R-,S-,T-,U-,V+,W-,X+,Y+,Z1}
+{$MINSTACKSIZE $00004000}
+{$MAXSTACKSIZE $00100000}
+{$IMAGEBASE $00400000}
+{$APPTYPE GUI}
+{$WARN SYMBOL_DEPRECATED ON}
+{$WARN SYMBOL_LIBRARY ON}
+{$WARN SYMBOL_PLATFORM ON}
+{$WARN UNIT_LIBRARY ON}
+{$WARN UNIT_PLATFORM ON}
+{$WARN UNIT_DEPRECATED ON}
+{$WARN HRESULT_COMPAT ON}
+{$WARN HIDING_MEMBER ON}
+{$WARN HIDDEN_VIRTUAL ON}
+{$WARN GARBAGE ON}
+{$WARN BOUNDS_ERROR ON}
+{$WARN ZERO_NIL_COMPAT ON}
+{$WARN STRING_CONST_TRUNCED ON}
+{$WARN FOR_LOOP_VAR_VARPAR ON}
+{$WARN TYPED_CONST_VARPAR ON}
+{$WARN ASG_TO_TYPED_CONST ON}
+{$WARN CASE_LABEL_RANGE ON}
+{$WARN FOR_VARIABLE ON}
+{$WARN CONSTRUCTING_ABSTRACT ON}
+{$WARN COMPARISON_FALSE ON}
+{$WARN COMPARISON_TRUE ON}
+{$WARN COMPARING_SIGNED_UNSIGNED ON}
+{$WARN COMBINING_SIGNED_UNSIGNED ON}
+{$WARN UNSUPPORTED_CONSTRUCT ON}
+{$WARN FILE_OPEN ON}
+{$WARN FILE_OPEN_UNITSRC ON}
+{$WARN BAD_GLOBAL_SYMBOL ON}
+{$WARN DUPLICATE_CTOR_DTOR ON}
+{$WARN INVALID_DIRECTIVE ON}
+{$WARN PACKAGE_NO_LINK ON}
+{$WARN PACKAGED_THREADVAR ON}
+{$WARN IMPLICIT_IMPORT ON}
+{$WARN HPPEMIT_IGNORED ON}
+{$WARN NO_RETVAL ON}
+{$WARN USE_BEFORE_DEF ON}
+{$WARN FOR_LOOP_VAR_UNDEF ON}
+{$WARN UNIT_NAME_MISMATCH ON}
+{$WARN NO_CFG_FILE_FOUND ON}
+{$WARN MESSAGE_DIRECTIVE ON}
+{$WARN IMPLICIT_VARIANTS ON}
+{$WARN UNICODE_TO_LOCALE ON}
+{$WARN LOCALE_TO_UNICODE ON}
+{$WARN IMAGEBASE_MULTIPLE ON}
+{$WARN SUSPICIOUS_TYPECAST ON}
+{$WARN PRIVATE_PROPACCESSOR ON}
+{$WARN UNSAFE_TYPE OFF}
+{$WARN UNSAFE_CODE OFF}
+{$WARN UNSAFE_CAST OFF}
 unit uMain;
 
 interface
@@ -16,21 +69,45 @@ type
     btnEmbOpen: TButton;
     vtOpen: TVirtualStringTree;
     spl1: TSplitter;
-    vtOutExt: TVirtualStringTree;
+    vtExts: TVirtualStringTree;
+    spl2: TSplitter;
+    pnlOutput: TPanel;
+    pnlOutputConfig: TPanel;
+    vtResult: TVirtualStringTree;
+    rgAdditionalDir: TRadioGroup;
+    grpTargetDir: TGroupBox;
+    rbInPlace: TRadioButton;
+    rbOutputDifferentDir: TRadioButton;
+    edtOutputDir: TEdit;
     procedure FormCreate(Sender: TObject);
     procedure btnEmbOpenClick(Sender: TObject);
     procedure vtOpenGetText(Sender: TBaseVirtualTree; Node: PVirtualNode;
       Column: TColumnIndex; TextType: TVSTTextType;
       var CellText: WideString);
-    procedure vtOutExtGetText(Sender: TBaseVirtualTree;
+    procedure vtExtsGetText(Sender: TBaseVirtualTree;
       Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
       var CellText: WideString);
-    procedure vtOutExtInitNode(Sender: TBaseVirtualTree; ParentNode,
+    procedure vtExtsInitNode(Sender: TBaseVirtualTree; ParentNode,
       Node: PVirtualNode; var InitialStates: TVirtualNodeInitStates);
+    procedure vtOpenGetNodeDataSize(Sender: TBaseVirtualTree;
+      var NodeDataSize: Integer);
+    procedure vtExtsGetNodeDataSize(Sender: TBaseVirtualTree;
+      var NodeDataSize: Integer);
+    procedure vtExtsChecked(Sender: TBaseVirtualTree;
+      Node: PVirtualNode);
+    procedure vtResultGetNodeDataSize(Sender: TBaseVirtualTree;
+      var NodeDataSize: Integer);
+    procedure vtResultGetText(Sender: TBaseVirtualTree; Node: PVirtualNode;
+      Column: TColumnIndex; TextType: TVSTTextType;
+      var CellText: WideString);
+    procedure edtOutputDirChange(Sender: TObject);
   private
     { Private declarations }
     FInputFiles : TStrings;
-    FOutputExts : TStrings;
+    //FOutputExts : TStrings;
+    procedure AddOpenFile(AFilePath: string);
+    procedure FillExts;
+    procedure RebuildOutputResult;
   public
     { Public declarations }
   end;
@@ -48,20 +125,57 @@ type
   PData = ^TData;
   TData = record
     processed : boolean;
-    fileName : string;
+    Txt : string;
   end;
 
+  PEmbExt = ^TEmbExt;
+  TEmbExt = record
+    Ext : string;
+    Desc : string;
+  end;
+
+  PResult = ^TResult;
+  TResult = record
+    Txt : string;
+    Source : string;
+    SourcePath : string;
+    processed : boolean;
+  end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  mmo1.Lines.Add( EmbReadersFilter() );
+  //mmo1.Lines.Add( EmbReadersFilter() );
   FInputFiles := TStringList.Create;
-  FOutputExts := TStringList.Create;
-  EmbFillWriter(FOutputExts);
-  vtOutExt.RootNodeCount := FOutputExts.Count;
-  self.dlgOpen1.Filter := EmbReadersFilter();
+  dlgOpen1.Filter := EmbReadersFilter();
+  FillExts();
+  pnlOutput.Align := alClient;
+end;
+
+procedure TForm1.FillExts();
+var
+  LExt : PEmbExt;
+  function AddNew(Parent: PVirtualNode; AExt, ADesc: string):PVirtualNode;
+  begin
+    result := vtExts.AddChild(Parent);
+    LExt := vtExts.GetNodeData(result);
+    LExt^.Ext := AExt;
+    LExt^.Desc := ADesc;
+  end;
+var i : integer;
+  LExtList : TStringList;
+begin
+  LExtList := TStringList.Create;
+  EmbFillWriter(LExtList);
+  //vtOutExt.RootNodeCount := LExtList.Count;
+  for i := 0 to LExtList.Count -1 do
+  begin
+    AddNew(vtExts.RootNode, '.'+LExtList.Names[i], LExtList.ValueFromIndex[i]);
+  end;
+  LExtList.Free;
 
 end;
+
+
 
 procedure TForm1.btnEmbOpenClick(Sender: TObject);
 var i : integer;
@@ -74,32 +188,240 @@ begin
   begin
     for i := 0 to dlgOpen1.Files.Count -1 do
     begin
-      if FInputFiles.IndexOf(dlgOpen1.Files[i]) < 0 then
-        FInputFiles.Add(dlgOpen1.Files[i]);
+      AddOpenFile(dlgOpen1.Files[i]);
     end;
-    vtOpen.RootNodeCount := FInputFiles.Count;
-
+    //vtOpen.RootNodeCount := FInputFiles.Count;
   end;
+  vtOpen.FullExpand();
 end;
+
+procedure TForm1.AddOpenFile(AFilePath: string);
+var
+  Data : PData;
+  function AddNew(Parent: PVirtualNode; AText: string):PVirtualNode;
+  begin
+    result := vtOpen.AddChild(Parent);
+    data := vtOpen.GetNodeData(result);
+    data^.Txt := Atext;
+    data^.processed := False;
+  end;
+
+var
+  LFile, LPath : string;
+  Parent, Node : PVirtualNode;
+  i : integer;
+begin
+  LFile := ExtractFileName(AFilePath);
+  LPath := ExtractFilePath(AFilePath);
+
+  //find parent
+  Parent := vtOpen.RootNode.FirstChild;
+  while Parent <> nil do
+  begin
+    Data := vtOpen.GetNodeData(Parent);
+    if Data^.Txt = LPath then
+    begin
+      break; //found.
+    end
+    else
+    begin
+      Parent := Parent.NextSibling;
+    end;
+  end;
+  if Not Assigned(Parent) then
+  begin
+    //append parent to root
+    Parent := AddNew(vtOpen.RootNode, LPath);
+  end;
+
+  //append child to parent
+  AddNew(Parent, LFile);
+end;
+
 
 procedure TForm1.vtOpenGetText(Sender: TBaseVirtualTree;
   Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
   var CellText: WideString);
+var
+  Data : PData;
 begin
-  CellText := FInputFiles[Node^.index];
+  Data := vtOpen.GetNodeData(Node);
+  CellText := Data^.Txt;// FInputFiles[Node^.index];
 end;
 
-procedure TForm1.vtOutExtGetText(Sender: TBaseVirtualTree;
+procedure TForm1.vtExtsGetText(Sender: TBaseVirtualTree;
   Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
   var CellText: WideString);
+var
+  Ext : PEmbExt;
 begin
-  CellText := FOutputExts[Node^.index];
+  Ext := vtExts.GetNodeData(Node);
+  if assigned(Ext) then
+  case Column of
+    0 : CellText := Ext^.Ext;
+    1 : CellText := Ext^.Desc;
+  end;
 end;
 
-procedure TForm1.vtOutExtInitNode(Sender: TBaseVirtualTree; ParentNode,
+procedure TForm1.vtExtsInitNode(Sender: TBaseVirtualTree; ParentNode,
   Node: PVirtualNode; var InitialStates: TVirtualNodeInitStates);
 begin
   Node^.CheckType := ctCheckBox;
+end;
+
+procedure TForm1.vtOpenGetNodeDataSize(Sender: TBaseVirtualTree;
+  var NodeDataSize: Integer);
+begin
+  NodeDataSize := sizeof(TData);
+end;
+
+procedure TForm1.vtExtsGetNodeDataSize(Sender: TBaseVirtualTree;
+  var NodeDataSize: Integer);
+begin
+  NodeDataSize := sizeof(TEmbExt);
+end;
+
+procedure TForm1.vtExtsChecked(Sender: TBaseVirtualTree;
+  Node: PVirtualNode);
+begin
+  RebuildOutputResult();
+end;
+
+procedure TForm1.RebuildOutputResult();
+var LExts : array of string;
+
+  procedure ScanExts();
+  var Node : PVirtualNode;
+    i : integer;
+    R : PEmbExt;
+  begin
+    SetLength(LExts,0);
+    Node := vtExts.RootNode;
+    repeat
+      Node := vtExts.GetNextChecked(Node, csCheckedNormal, true);
+      if assigned(Node) then
+      begin
+        i := Length(LExts);
+        SetLength(LExts, i+1);
+        R := vtExts.GetNodeData(Node);
+        LExts[i] := '.'+ R^.Ext;
+      end;
+    until Node = nil;
+
+  end;
+
+  var
+    RData : PResult;
+  function AddNew(Parent: PVirtualNode; AText: string; ASource: string=''; APath: string=''):PVirtualNode;
+  begin
+    result := vtResult.AddChild(Parent);
+    RData := vtResult.GetNodeData(result);
+    RData^.Txt := Atext;
+    RData^.processed := False;
+    RData^.Source := ASource;
+    RData^.SourcePath := APath;
+  end;
+  procedure AttachManyExt(Parent: PVirtualNode; AText: string; ASource: string=''; APath: string='');
+  begin
+
+  end;
+
+  procedure AddNewResult(AFile, APath: string);
+  var Parent : PVirtualNode;
+    i : integer;
+  begin
+    //find parent
+    Parent := vtResult.RootNode.FirstChild;
+    while Parent <> nil do
+    begin
+      RData := vtResult.GetNodeData(Parent);
+      if RData^.Txt = APath then
+      begin
+        break; //found.
+      end
+      else
+      begin
+        Parent := Parent.NextSibling;
+      end;
+    end;
+    if Not Assigned(Parent) then
+    begin
+      //append parent to root
+      Parent := AddNew(vtResult.RootNode, APath);
+    end;
+
+    //append child to parent
+    for i := 0 to length(LExts) -1 do
+    begin
+      AddNew(Parent, ChangeFileExt(AFile, LExts[i]), AFile, APath );
+    end;
+  end;
+
+var
+  Parent, Node, RNode : PVirtualNode;
+  R : PResult;
+  ParentData, Data : PData;
+
+begin
+  //result count = checked.count * open.count
+  vtResult.BeginUpdate;
+  try
+    vtResult.Clear;
+    ScanExts();
+    Parent := vtOpen.RootNode.FirstChild;
+    while Parent <> nil do
+    begin
+      ParentData := vtOpen.GetNodeData(Parent);
+
+      Node := Parent.FirstChild;
+      while Node <> nil do
+      begin
+        Data := vtOpen.GetNodeData(Node);
+        {RNode := vtResult.AddChild(nil);
+        R := vtResult.GetNodeData(RNode);
+        R^.Txt := Data^.Txt;}
+        AddNewResult(Data^.Txt, ParentData^.Txt);
+        Node := Node.NextSibling;
+      end;
+      
+      {Node := vtExts.GetNextChecked(Node, csCheckedNormal, true);
+      if assigned(Node) then
+      begin
+        i := Length(LExts);
+        SetLength(LExts, i+1);
+        R := vtExts.GetNodeData(Node);
+        LExts[i] := R^.Ext;
+      end;}
+      Parent := Parent.NextSibling;
+    end;
+  finally
+    vtResult.FullExpand();
+    vtResult.EndUpdate;
+  end;
+end;
+
+procedure TForm1.vtResultGetNodeDataSize(Sender: TBaseVirtualTree;
+  var NodeDataSize: Integer);
+begin
+  NodeDataSize := sizeOf(TResult);
+end;
+
+procedure TForm1.vtResultGetText(Sender: TBaseVirtualTree;
+  Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
+  var CellText: WideString);
+var Data : PResult;
+begin
+  data := vtResult.GetNodeData(Node);
+  case column of
+    0 : CellText := data^.Txt;
+    1 : CellText := data^.Source;
+    2 : CellText := data^.SourcePath;
+  end;
+end;
+
+procedure TForm1.edtOutputDirChange(Sender: TObject);
+begin
+  rbOutputDifferentDir.Checked := true;
 end;
 
 end.
